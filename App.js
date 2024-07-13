11dfaa02c418b3dbfd5b81e5a349c50c78313a8f
emanuelnakeng <emanuelnakeng@gmail.com>
2024-07-13 04:58:36 +0200

    App stable

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import OnBoarding from './screens/OnBoarding';
+import { useFonts } from 'expo-font';
+import { useCallback, useEffect, useState } from 'react';
+import constants from './utils/constants';
+import AuthStack from './utils/nav/AuthStack';
+import AppStack from './utils/nav/AppStack';
+import CardContextProvider from './utils/cardContextAPI';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [isInitializing, setIsInitializing] = useState(true);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	const [fontsLoaded, fontError] = useFonts({
+		inter: constants.FONT,
+	});
+
+	useEffect(() => {
+		if (fontsLoaded) {
+			setIsInitializing(false);
+		}
+	}, []);
+
+	if (fontError) {
+		return null;
+	}
+
+	return (
+		<NavigationContainer>
+			<CardContextProvider>
+				<AppStack />
+			</CardContextProvider>
+		</NavigationContainer>
+	);
+}
